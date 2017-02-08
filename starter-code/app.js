0c145ee53c12538ca4a32c6a5e0a490df25e71fa
alexola <alexola.22@gmail.com>
2017-02-08 18:24:06 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,52 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
 
+
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res) => {
+
+    client.getRandomJoke().then(function(response) {
+        let data = {};
+        console.log(response.value);
+        data.random = response.value;
+        res.render('random', data);
+    }).catch(function(err) {
+        res.send(err);
+    });
+});
+
+app.get('/categories', (req, res) => {
+
+    client.getJokeCategories().then(function(categories) {
+        console.log(categories);
+        let cat = {};
+        cat.catList = categories;
+        console.log(cat);
+        res.render('categories', cat);
+    }).catch(function(err) {
+        res.send(err);
+    });
+});
+
+
+app.get('/categories/:category', (req, res) => {
+
+    client.getRandomJoke(req.params.category).then(function(response) {
+        let data = {};
+        console.log(response.value);
+        data.random = response.value;
+        res.render('joke-by-category', data);
+    }).catch(function(err) {
+        res.send(err);
+    });
+
+});
+
+
+app.listen(3000, () => {
+    console.log('My first app listening on port 3000!')
+});
